bc09bcf8650db6ae28d8ecb718cfe3cf47090ab7
Rhett Muse <rhett.muse123@gmail.com>
2023-05-22 11:35:23 -0500

    define kiln then firepottery in main

---
+++ src/scripts/Kiln.js
@@ -0,0 +1,9 @@
+export const firePottery = (pottery, temp) => {
+    pottery.fired = true;
+    if (temp > 2200) {
+        pottery.cracked = true;
+    } else {
+        pottery.cracked = false;
+    }
+    return pottery;
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import { makePottery } from './PotteryWheel.js'
+import { firePottery } from './Kiln.js'
 // Imports go first
 
 
@@ -8,10 +9,15 @@ let cup = makePottery("Cup", 1, 2)
 let bowl = makePottery("Bowl", 3, 4)
 let vase = makePottery("Vase", 12, 7)
 let cat = makePottery("Cat", 7, 5)
-console.log([mug, cup, bowl, vase, cat])
+// console.log([mug, cup, bowl, vase, cat])
 
 // Fire each piece of pottery in the kiln
-
+firePottery(mug, 2100)
+firePottery(cup, 2201)
+firePottery(bowl, 2100)
+firePottery(vase, 2100)
+firePottery(cat, 2100)
+console.log([mug, cup, bowl, vase, cat])
 
 // Determine which ones should be sold, and their price
 
