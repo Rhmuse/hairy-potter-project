3cd7865aba8dde4880dc02609168310688c62730
Rhett Muse <rhett.muse123@gmail.com>
2023-05-22 11:23:36 -0500

    make 5 pieces of pottery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,14 @@
+import { makePottery } from './PotteryWheel.js'
 // Imports go first
 
 
-
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-
+let mug = makePottery("Mug", 1, 2)
+let cup = makePottery("Cup", 1, 2)
+let bowl = makePottery("Bowl", 3, 4)
+let vase = makePottery("Vase", 12, 7)
+let cat = makePottery("Cat", 7, 5)
+console.log([mug, cup, bowl, vase, cat])
 
 // Fire each piece of pottery in the kiln
 
